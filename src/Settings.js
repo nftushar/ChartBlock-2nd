9a30bd9dd5106ec75f58493a556c6ad924d264d5
nftushar <nftushar5200@gmail.com>
2023-11-07 14:41:31 +0600

    Basic fiters done

--- src/Settings.js
+++ src/Settings.js
@@ -1,6 +1,6 @@
 import { __ } from "@wordpress/i18n";
 import { InspectorControls } from "@wordpress/block-editor";
-import { PanelBody, TabPanel, SelectControl, RangeControl, __experimentalNumberControl as NumberControl, __experimentalBoxControl as BoxControl, __experimentalUnitControl as UnitControl } from "@wordpress/components";
+import { PanelBody, TabPanel, SelectControl, RangeControl, TextControl, __experimentalBoxControl as BoxControl, __experimentalUnitControl as UnitControl } from "@wordpress/components";
 import { BColor, InlineMediaUpload } from "../../Components";
 import useFileData from './hooks/useFileData';
 
@@ -21,11 +21,11 @@ const Settings = ({ attributes, setAttributes }) => {
 
   const { file, chart, padding } = attributes;
   // console.log(padding);
-  const { type, border, radius, chartWidth, chartHeight, background, backgroundColor, borderColor, } = chart;
+  const { title, type, border, radius, chartWidth, chartHeight, background, backgroundColor, borderColor, } = chart;
 
   const { fetchData } = useFileData(file);
 
-  let { data } = useFileData(file); 
+  let { data } = useFileData(file);
 
   const updateChart = (property, value, index) => {
     const newChart = { ...chart };
@@ -51,6 +51,16 @@ const Settings = ({ attributes, setAttributes }) => {
                 className="bPlPanelBody"
                 title={__("Settings", "pie-chart")} >
 
+                <TextControl
+                  label={__("Additional CSS Class)", "pie-chart")}
+                  value={title}
+                  onChange={(val) =>
+                    setAttributes({
+                      chart: { ...chart, title: val },
+                    })
+                  } 
+                />
+
                 <InlineMediaUpload value={file}
                   label={__("Upload File ( JSON, XML, CSV )", "pie-chart")}
                   types={['application/json', 'application/rss+xml', 'text/csv']}
@@ -59,7 +69,6 @@ const Settings = ({ attributes, setAttributes }) => {
                     fetchData(val).then((data) => {
                       setAttributes(data);
                       const generatedColors = data.map(() => getRandomColor());
-
                       setAttributes({ chart: { ...chart, backgroundColor: generatedColors.map(c => `${c}33`), borderColor: generatedColors } });
                     }).catch((error) => {
                       // eslint-disable-next-line no-console
